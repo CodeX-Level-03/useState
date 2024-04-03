fcbc931fc4c1dd3615b6b752a3fe32d638acd005
uxeeCoder <umar.afar@aol.com>
2024-04-02 22:36:08 -0400

    file created to make a counter on the webpage

--- src/app/counter.jsx
+++ src/app/counter.jsx
@@ -1,10 +1,23 @@
+"use client";
+import React, { useState } from "react";
 
-'use client'   ;
-import React from 'react';
-
-export default function counter (){
-
-    return (
-        <div>
-    )
+export default function counter() {
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+  return (
+    <>
+      <h2>counter: {counter}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
+    </>
+  );
 }
